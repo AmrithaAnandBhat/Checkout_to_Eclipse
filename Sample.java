3320e944193df078e0d94fe414e7f5afbe429f2b
Amritha <amrithaanand13@gmail.com>
2023-11-13 12:21:56 -0600

    Changing the sample.java file contents

--- Sample.java
+++ Sample.java
@@ -1,1 +1,14 @@
-Hi.....Good Day!.....Welcome to GitHub Integration with Eclipse!!!
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("example java class");
+		 System.out.println(100 + 100 +"Simplilearn"); 
+			        System.out.println("E-Learning Company" + 100 + 100);  
+
+	}
+
+}
